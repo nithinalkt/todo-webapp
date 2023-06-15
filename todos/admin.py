e2b201da6457b39c857531c1550b00b3db7caa24
nithin <nithinalkt4@gmail.com>
2023-06-15 13:28:18 +0530

    New updates for Heroku deployment

--- todos/admin.py
+++ todos/admin.py
@@ -1,3 +1,9 @@
+# todos/admin.py
 from django.contrib import admin
-
-# Register your models here.
+from .models import Todo
+class TodoAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "body",
+)
+admin.site.register(Todo,TodoAdmin)
--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
-# Create your views here.
+# todos/views.py
+from rest_framework import generics
+from .models import Todo
+from .serializers import TodoSerializer
+class ListTodo(generics.ListAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+class DetailTodo(generics.RetrieveAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
